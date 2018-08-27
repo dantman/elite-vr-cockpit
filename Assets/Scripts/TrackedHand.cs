938a10d4267c6b2f1bde5af66b1480a74bd0c0e6
Daniel Friesen <d@danf.ca>
2018-08-27 01:18:44 -0700

    Setup base for the Elite VR Cockpit overlay
    - Unity project with SteamVR Plugin
    - OverlayController (openvr init, etc) and EventLoop handler, to setup overlay mode and replace some SteamVR Plugin functionality
    - A TrackedHand behaviour that makes a game object follow the tracked controller

---
+++ Assets/Scripts/TrackedHand.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+public class TrackedHand : MonoBehaviour
+{
+    public enum Hand
+    {
+        Left,
+        Right,
+    }
+
+    public Hand hand = Hand.Left;
+
+    private uint deviceIndex = OpenVR.k_unTrackedDeviceIndexInvalid;
+
+    public ETrackedControllerRole controllerRole
+    {
+        get
+        {
+            return hand == Hand.Right
+                ? ETrackedControllerRole.RightHand
+                : ETrackedControllerRole.LeftHand;
+        }
+    }
+
+    public bool hasValidDevice
+    {
+        get
+        {
+            if (deviceIndex == OpenVR.k_unTrackedDeviceIndexInvalid)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    SteamVR_Events.Action newPosesAction;
+
+    TrackedHand()
+    {
+        newPosesAction = SteamVR_Events.NewPosesAction(OnNewPoses);
+    }
+
+    void OnEnable()
+    {
+        SteamVR_Events.System(EVREventType.VREvent_TrackedDeviceActivated).Listen(OnTrackedDeviceEvent);
+        SteamVR_Events.System(EVREventType.VREvent_TrackedDeviceDeactivated).Listen(OnTrackedDeviceEvent);
+        SteamVR_Events.System(EVREventType.VREvent_TrackedDeviceUpdated).Listen(OnTrackedDeviceEvent);
+        SteamVR_Events.System(EVREventType.VREvent_TrackedDeviceRoleChanged).Listen(OnTrackedDeviceEvent);
+        newPosesAction.enabled = true;
+
+        RescanDevices();
+    }
+
+    void OnDisable()
+    {
+        SteamVR_Events.System(EVREventType.VREvent_TrackedDeviceActivated).Remove(OnTrackedDeviceEvent);
+        SteamVR_Events.System(EVREventType.VREvent_TrackedDeviceDeactivated).Remove(OnTrackedDeviceEvent);
+        SteamVR_Events.System(EVREventType.VREvent_TrackedDeviceUpdated).Remove(OnTrackedDeviceEvent);
+        SteamVR_Events.System(EVREventType.VREvent_TrackedDeviceRoleChanged).Remove(OnTrackedDeviceEvent);
+        newPosesAction.enabled = false;
+    }
+
+    void OnTrackedDeviceEvent(VREvent_t ev)
+    {
+        RescanDevices();
+    }
+
+    // Scan the tracked device list to find the left or right hand
+    void RescanDevices()
+    {
+        deviceIndex = OpenVR.k_unTrackedDeviceIndexInvalid;
+
+        var vr = OpenVR.System;
+        if (vr == null) return;
+
+        deviceIndex = vr.GetTrackedDeviceIndexForControllerRole(controllerRole);
+    }
+
+    void OnNewPoses(TrackedDevicePose_t[] poses)
+    {
+        if (!hasValidDevice) return;
+
+        if (poses.Length < deviceIndex) return;
+
+        if (!poses[deviceIndex].bDeviceIsConnected) return;
+
+        if (!poses[deviceIndex].bPoseIsValid) return;
+
+        var pose = new SteamVR_Utils.RigidTransform(poses[deviceIndex].mDeviceToAbsoluteTracking);
+
+        transform.localPosition = pose.pos;
+        transform.localRotation = pose.rot;
+    }
+
+    private void Update()
+    {
+        if (!hasValidDevice) return;
+    }
+}
