b93094d6c896ad3700011bc8b67985fd99a54dc6
Daniel Friesen <d@danf.ca>
2018-08-30 01:18:29 -0700

    Watch the Status.json and make a manager show/hide game objects based on the current state

---
+++ Assets/Scripts/CockpitUIMode.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using UnityEngine;
+
+namespace EVRC
+{
+    using EDGuiFocus = EDStateManager.EDStatus_GuiFocus;
+    using EDStatus_Flags = EDStateManager.EDStatus_Flags;
+
+    /**
+     * Behaviour that keeps track of the ED state and enables/disables game objects
+     * depending on the state (menu, piloting, galaxy/system map, ship, SRV, etc)
+     */
+    public class CockpitUIMode : MonoBehaviour
+    {
+        public GameObject gameNotRunning;
+        public GameObject map;
+        public GameObject cockpit;
+        public GameObject shipOnlyCockpit;
+        public GameObject srvOnlyCockpit;
+        private EDGuiFocus GuiFocus;
+        private EDStatus_Flags StatusFlags;
+
+        [Flags]
+        public enum CockpitMode : byte
+        {
+            GameNotRunning = 0x01,
+            InGame = 0x02,
+            Map = 0x04,
+            Cockpit = 0x08,
+            InMainShip = 0x10,
+            InSRV = 0x20,
+        }
+
+        void OnEnable()
+        {
+            EDStateManager.EliteDangerousStarted.Listen(OnGameStartedOrStopped);
+            EDStateManager.EliteDangerousStopped.Listen(OnGameStartedOrStopped);
+            EDStateManager.GuiFocusChanged.Listen(OnGuiFocusChanged);
+            EDStateManager.FlagsChanged.Listen(OnFlagsChanged);
+            Refresh();
+        }
+
+
+        void OnDisable()
+        {
+            EDStateManager.EliteDangerousStarted.Remove(OnGameStartedOrStopped);
+            EDStateManager.EliteDangerousStopped.Remove(OnGameStartedOrStopped);
+            EDStateManager.GuiFocusChanged.Remove(OnGuiFocusChanged);
+            EDStateManager.FlagsChanged.Listen(OnFlagsChanged);
+        }
+
+        private void OnGameStartedOrStopped()
+        {
+            Refresh();
+        }
+
+        void OnGuiFocusChanged(EDGuiFocus guiFocus)
+        {
+            GuiFocus = guiFocus;
+            Refresh();
+        }
+
+        private void OnFlagsChanged(EDStatus_Flags flags)
+        {
+            StatusFlags = flags;
+            Refresh();
+        }
+
+        void Refresh()
+        {
+            if (!EDStateManager.instance.IsEliteDangerousRunning)
+            {
+                if (gameNotRunning != null) gameNotRunning.SetActive(true);
+                SetMode(CockpitMode.GameNotRunning);
+                return;
+            }
+
+            var mode = CockpitMode.InGame;
+
+            if (GuiFocus == EDGuiFocus.GalaxyMap || GuiFocus == EDGuiFocus.SystemMap)
+            {
+                mode |= CockpitMode.Map;
+            }
+            else
+            {
+                mode |= CockpitMode.Cockpit;
+                if (StatusFlags.HasFlag(EDStatus_Flags.InMainShip))
+                {
+                    mode |= CockpitMode.InMainShip;
+                }
+                if (StatusFlags.HasFlag(EDStatus_Flags.InSRV))
+                {
+                    mode |= CockpitMode.InSRV;
+                }
+            }
+
+            SetMode(mode);
+        }
+
+        void SetMode(CockpitMode mode)
+        {
+            if (gameNotRunning)
+            {
+                gameNotRunning.SetActive(mode.HasFlag(CockpitMode.GameNotRunning));
+            }
+            if (map)
+            {
+                map.SetActive(mode.HasFlag(CockpitMode.Map));
+            }
+            if (cockpit)
+            {
+                cockpit.SetActive(mode.HasFlag(CockpitMode.Cockpit));
+            }
+            if (shipOnlyCockpit)
+            {
+                shipOnlyCockpit.SetActive(mode.HasFlag(CockpitMode.Cockpit) && mode.HasFlag(CockpitMode.InMainShip));
+            }
+            if (srvOnlyCockpit)
+            {
+                srvOnlyCockpit.SetActive(mode.HasFlag(CockpitMode.Cockpit) && mode.HasFlag(CockpitMode.InSRV));
+            }
+        }
+    }
+}
--- Assets/Scripts/EDStateManager.cs
+++ Assets/Scripts/EDStateManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Text.RegularExpressions;
 using System.Linq;
 using System.IO;
@@ -22,37 +23,122 @@ namespace EVRC
             }
         }
 
+        [Flags]
+        public enum EDStatus_Flags : uint
+        {
+            Docked = 0x00000001,
+            Landed = 0x00000002,
+            LandingGearDown = 0x00000004,
+            ShieldsUp = 0x00000008,
+            Supercruise = 0x00000010,
+            FlightAssistOff = 0x00000020,
+            HardpointsDeployed = 0x00000040,
+            InWing = 0x00000080,
+            LightsOn = 0x00000100,
+            CargoScoopDeployed = 0x00000200,
+            SilentRunning = 0x00000400,
+            ScoopingFuel = 0x00000800,
+            SrvHandbrake = 0x00001000,
+            SrvTurret = 0x00002000,
+            SrvUnderShip = 0x00004000,
+            SrvDriveAssist = 0x00008000,
+            FsdMassLocked = 0x00010000,
+            FsdCharging = 0x00020000,
+            FsdCooldown = 0x00040000,
+            LowFuel = 0x00080000,
+            OverHeating = 0x00100000,
+            HasLatLong = 0x00200000,
+            IsInDanger = 0x00400000,
+            BeingInterdicted = 0x00800000,
+            InMainShip = 0x01000000,
+            InFighter = 0x02000000,
+            InSRV = 0x04000000,
+        }
+
+        public enum EDStatus_GuiFocus : byte
+        {
+            NoFocus = 0,
+            InternalPanel = 1,
+            ExternalPanel = 2,
+            CommsPanel = 3,
+            RolePanel = 4,
+            StationServices = 5,
+            GalaxyMap = 6,
+            SystemMap = 7,
+            Unknown = byte.MaxValue
+        }
+
+        public struct EDStatus
+        {
+            public string timestamp;
+            public uint Flags;
+            public byte[] Pips;
+            public byte FireGroup;
+            public byte GuiFocus;
+        }
+
         public static readonly string EDProcessName32 = "EliteDangerous32";
         public static readonly string EDProcessName64 = "EliteDangerous64";
         private uint currentPid;
-        public bool isEliteDangerousRunning { get; private set; } = false;
+        public bool IsEliteDangerousRunning { get; private set; } = false;
         public HudColorMatrix hudColorMatrix { get; private set; } = HudColorMatrix.Identity();
 
+        public static Events.Event EliteDangerousStarted = new Events.Event();
+        public static Events.Event EliteDangerousStopped = new Events.Event();
+        public static Events.Event<HudColorMatrix> HudColorMatrixChanged = new Events.Event<HudColorMatrix>();
+
+        public EDStatus? LastStatus { get; private set; } = null;
+        public EDStatus_GuiFocus GuiFocus { get; private set; } = EDStatus_GuiFocus.NoFocus;
+        public EDStatus_Flags StatusFlags { get; private set; }
+
+        public static Events.Event<EDStatus, EDStatus?> StatusChanged = new Events.Event<EDStatus, EDStatus?>();
+        public static Events.Event<EDStatus_GuiFocus> GuiFocusChanged = new Events.Event<EDStatus_GuiFocus>();
+        public static Events.Event<EDStatus_Flags> FlagsChanged = new Events.Event<EDStatus_Flags>();
+
+        private static string _appDataPath;
+        public static string AppDataPath
+        {
+            get
+            {
+                if (_appDataPath == null)
+                {
+                    var LocalAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                    _appDataPath = Path.Combine(LocalAppData, "Frontier Developments", "Elite Dangerous");
+                }
+
+                return _appDataPath;
+            }
+        }
+
         private static string _saveDataPath;
-        public static string saveDataPath
+        public static string SaveDataPath
         {
             get
             {
                 if (_saveDataPath == null)
                 {
-                    var LocalAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-                    _saveDataPath = Path.Combine(LocalAppData, "Frontier Developments", "Elite Dangerous");
+                    var userDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                    _saveDataPath = Path.Combine(userDir, "Saved Games", "Frontier Developments", "Elite Dangerous");
                 }
 
                 return _saveDataPath;
             }
         }
-        public static string graphicsConfigurationOverridePath
+
+        public static string GraphicsConfigurationOverridePath
         {
             get
             {
-                return Path.Combine(saveDataPath, "Options", "Graphics", "GraphicsConfigurationOverride.xml");
+                return Path.Combine(AppDataPath, "Options", "Graphics", "GraphicsConfigurationOverride.xml");
             }
         }
-
-        public static Events.Event EliteDangerousStarted = new Events.Event();
-        public static Events.Event EliteDangerousStopped = new Events.Event();
-        public static Events.Event<HudColorMatrix> HudColorMatrixChanged = new Events.Event<HudColorMatrix>();
+        public static string StatusFilePath
+        {
+            get
+            {
+                return Path.Combine(SaveDataPath, "Status.json");
+            }
+        }
 
         void Start()
         {
@@ -106,17 +192,19 @@ namespace EVRC
 
         private void SetIsEliteDangerousRunning(bool running)
         {
-            if (isEliteDangerousRunning == running) return;
-            isEliteDangerousRunning = running;
+            if (IsEliteDangerousRunning == running) return;
+            IsEliteDangerousRunning = running;
 
-            if (isEliteDangerousRunning)
+            if (IsEliteDangerousRunning)
             {
                 LoadHUDColorMatrix(); // Reload the HUD color matrix on start
+                StartCoroutine(WatchStatusFile());
                 EliteDangerousStarted.Send();
             }
             else
             {
                 EliteDangerousStopped.Send();
+                LastStatus = null;
             }
         }
 
@@ -125,7 +213,7 @@ namespace EVRC
          */
         private void LoadHUDColorMatrix()
         {
-            var doc = XDocument.Load(graphicsConfigurationOverridePath);
+            var doc = XDocument.Load(GraphicsConfigurationOverridePath);
             var defaultGuiColor = doc.Descendants("GUIColour").Descendants("Default");
             var RedLine = (from el in defaultGuiColor.Descendants("MatrixRed") select el).First().Value;
             var GreenLine = (from el in defaultGuiColor.Descendants("MatrixGreen") select el).First().Value;
@@ -149,6 +237,46 @@ namespace EVRC
         public static Color ApplyHudColorMatrix(Color color)
         {
             return instance.hudColorMatrix.Apply(color);
+        }
+
+        private IEnumerator WatchStatusFile()
+        {
+            while (IsEliteDangerousRunning)
+            {
+                try
+                {
+                    var status = JsonUtility.FromJson<EDStatus>(File.ReadAllText(StatusFilePath));
+
+                    if (LastStatus == null || status.timestamp != LastStatus.Value.timestamp)
+                    {
+                        StatusChanged.Send(status, LastStatus);
+
+                        if (LastStatus == null || LastStatus.Value.GuiFocus != status.GuiFocus)
+                        {
+                            var guiFocus = Enum.IsDefined(typeof(EDStatus_GuiFocus), status.GuiFocus)
+                                ? (EDStatus_GuiFocus)status.GuiFocus
+                                : EDStatus_GuiFocus.Unknown;
+
+                            GuiFocus = guiFocus;
+                            GuiFocusChanged.Send(guiFocus);
+                        }
+
+                        if (LastStatus == null || LastStatus.Value.Flags != status.Flags)
+                        {
+                            var Flags = (EDStatus_Flags)status.Flags;
+                            FlagsChanged.Send(Flags);
+                        }
+
+                        LastStatus = status;
+                    }
+                }
+                catch (IOException)
+                {
+                    // Ignore IO exceptions, these might be caused by inevitably reading while ED is writing
+                }
+
+                yield return new WaitForSecondsRealtime(1f);
+            }
         }
     }
 }
