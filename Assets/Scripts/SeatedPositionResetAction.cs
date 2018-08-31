86e85bbb682c0b3b85c142b071ca40308145bc8e
Daniel Friesen <d@danf.ca>
2018-08-30 21:14:14 -0700

    Add a behaviour that resets the seated position when you hold down both menu buttons for 3 seconds

---
+++ Assets/Scripts/SeatedPositionResetAction.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using UnityEngine;
+using Valve.VR;
+
+namespace EVRC
+{
+    public class SeatedPositionResetAction : MonoBehaviour
+    {
+        public float holdForSeconds = 1f;
+        private bool leftMenuPressed = false;
+        private bool rightMenuPressed = false;
+
+        void OnEnable()
+        {
+            ActionsController.MenuPress.Listen(OnMenuPress);
+            ActionsController.MenuUnpress.Listen(OnMenuUnpress);
+        }
+
+        void OnDisable()
+        {
+            ActionsController.MenuPress.Remove(OnMenuPress);
+            ActionsController.MenuUnpress.Remove(OnMenuUnpress);
+        }
+
+        private void OnMenuPress(ActionsController.ButtonPress ev)
+        {
+            switch (ev.hand)
+            {
+                case ActionsController.Hand.Left:
+                    leftMenuPressed = true;
+                    break;
+                case ActionsController.Hand.Right:
+                    rightMenuPressed = true;
+                    break;
+            }
+
+            if (leftMenuPressed && rightMenuPressed)
+            {
+                StartCoroutine(HoldingMenuButtons());
+            }
+        }
+
+        private void OnMenuUnpress(ActionsController.ButtonPress ev)
+        {
+            switch (ev.hand)
+            {
+                case ActionsController.Hand.Left:
+                    leftMenuPressed = false;
+                    break;
+                case ActionsController.Hand.Right:
+                    rightMenuPressed = false;
+                    break;
+            }
+
+            StopAllCoroutines();
+        }
+
+        private IEnumerator HoldingMenuButtons()
+        {
+            yield return new WaitForSeconds(holdForSeconds);
+
+            if (leftMenuPressed && rightMenuPressed)
+            {
+                OpenVR.System.ResetSeatedZeroPose();
+            }
+        }
+    }
+}
