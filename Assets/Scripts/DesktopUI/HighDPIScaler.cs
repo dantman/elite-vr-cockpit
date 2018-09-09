a5b603606b0c81ff594d49c3984ec70cdba9a8ac
Daniel Friesen <d@danf.ca>
2018-09-09 16:52:00 -0700

    Add a HighDPI scaler & rename label Console -> Log
    HighDPI scaler checks Screen.dpi and uses it to adjust a canvas' scale.

---
+++ Assets/Scripts/DesktopUI/HighDPIScaler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace EVRC.DesktopUI
+{
+    [RequireComponent(typeof(CanvasScaler))]
+    public class HighDPIScaler : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            var scaler = GetComponent<CanvasScaler>();
+#if UNITY_EDITOR
+            // The Unity Editor's game view already sets a 3x scale factor so don't scale further
+            scaler.scaleFactor = 1;
+#else
+            // Scale the UI so it is still readable on HighDPI/4K screens
+            scaler.scaleFactor = Mathf.Round(Screen.dpi / 96f);
+#endif
+        }
+    }
+}
