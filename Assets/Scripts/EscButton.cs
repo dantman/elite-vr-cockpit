c6725b26317d6c6e97e36f09723185a94fb7817f
Daniel Friesen <d@danf.ca>
2018-09-09 19:53:24 -0700

    Implement an Esc key button (non-functional)
    For some reason ED isn't catching our Escape key presses the way it catches our other key presses.
    For now leave it disabled.

---
+++ Assets/Scripts/EscButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace EVRC
+{
+    public class EscButton : BaseButton
+    {
+        public override void Activate()
+        {
+            if (!KeyboardInterface.Send("Key_Escape"))
+            {
+                Debug.LogWarning("Could not send keypress Key_Escape, did not understand the key");
+            }
+        }
+    }
+}
