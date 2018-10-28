2672095ff412528f6a8ad984b552f2f26099fd1e
Daniel Friesen <d@danf.ca>
2018-10-27 19:18:14 -0700

    Implement a controls panel UI for setting joystick POV bindings

---
+++ Assets/Scripts/AlphaRaycastButtonImage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace EVRC
+{
+    [RequireComponent(typeof(Image))]
+    public class AlphaRaycastButtonImage : MonoBehaviour
+    {
+        [Range(0f, 1f)]
+        public float alphaHitTestMinimumThreshold = 0f;
+
+        private void Start()
+        {
+            Image image = GetComponent<Image>();
+            if (image)
+            {
+                image.alphaHitTestMinimumThreshold = alphaHitTestMinimumThreshold;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EmitPOVButton.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace EVRC
+{
+    using HatDirection = vJoyInterface.HatDirection;
+
+    /**
+     * Helper that emits dummy button presses for POV buttons and directions
+     */
+    public class EmitPOVButton : MonoBehaviour
+    {
+        public vJoyInterface output;
+        [Range(1, 4)]
+        public uint hatNumber = 1;
+        public uint buttonNumber = 1;
+
+        public void Direction(HatDirection direction)
+        {
+            StartCoroutine(EmitDirection(direction));
+        }
+        public void Direction(string direction)
+        {
+            HatDirection hatDirection;
+            if (Enum.TryParse(direction, out hatDirection))
+            {
+                Direction(hatDirection);
+            }
+            else
+            {
+                Debug.LogErrorFormat("Invalid direction: {0}", direction);
+            }
+        }
+
+        public void Button()
+        {
+            StartCoroutine(EmitButton());
+        }
+
+        public IEnumerator EmitDirection(HatDirection direction)
+        {
+            output.SetHatDirection(hatNumber, direction);
+            yield return new WaitForSecondsRealtime(0.1f);
+            output.SetHatDirection(hatNumber, HatDirection.Neutral);
+        }
+
+        public IEnumerator EmitButton()
+        {
+            output.SetButton(buttonNumber, true);
+            yield return new WaitForSecondsRealtime(0.1f);
+            output.SetButton(buttonNumber, false);
+        }
+    }
+}
