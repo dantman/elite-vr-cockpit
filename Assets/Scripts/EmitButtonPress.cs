d577123254a0894778bf550d60e5faed7c5e41ec
Daniel Friesen <d@danf.ca>
2018-10-27 22:51:12 -0700

    Implement UI to bind primary/secondary/alt i.e. trigger, application menu/b/y, a/x buttons for the joystick and throttle
    Also top off the POV implementation

---
+++ Assets/Scripts/EmitButtonPress.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace EVRC
+{
+    /**
+     * Helper that emits a dummy button press
+     */
+    public class EmitButtonPress : MonoBehaviour
+    {
+        public vJoyInterface output;
+        public uint buttonNumber = 1;
+
+        public void Press()
+        {
+            StartCoroutine(Emit());
+        }
+
+        public IEnumerator Emit()
+        {
+            output.SetButton(buttonNumber, true);
+            yield return new WaitForSecondsRealtime(0.1f);
+            output.SetButton(buttonNumber, false);
+        }
+    }
+}
