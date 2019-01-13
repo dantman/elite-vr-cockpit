2ca2f323272fee1d1b88edb3102cbb15c84d656a
Daniel Friesen <d@danf.ca>
2019-01-12 18:23:13 -0800

    Auto-release pressed joystick/throttle buttons and POV directions when letting go
    Previously they would stay pressed if you let go before you released the button.
    
    This also refactors Virtutal{Joystick/Throttle}Buttons to use a common VirtualControlButtons base class.

--- Assets/Scripts/VirtualThrottleButtons.cs
+++ Assets/Scripts/VirtualThrottleButtons.cs
@@ -10,17 +10,14 @@ namespace EVRC
     /**
      * Outputs joystick buttons to vJoy when the associated throttle is grabbed
      */
-    public class VirtualThrottleButtons : MonoBehaviour
+    public class VirtualThrottleButtons : VirtualControlButtons
     {
-        public vJoyInterface output;
-        // Map of abstractes BtnAction presses to vJoy joystick button numbers
+        // Map of abstracted BtnAction presses to vJoy joystick button numbers
         private static Dictionary<BtnAction, uint> joyBtnMap = new Dictionary<BtnAction, uint>()
         {
             { BtnAction.Trigger, 8 },
             { BtnAction.Secondary, 7 },
         };
-        // The hand of the controller grabbing the joystick, Unknown is considered "not grabbing"
-        private Hand grabbedHand = Hand.Unknown;
 
         private void OnEnable()
         {
@@ -30,49 +27,29 @@ namespace EVRC
 
         private void OnDisable()
         {
-
             ActionsController.ButtonActionPress.Remove(OnActionPress);
             ActionsController.ButtonActionUnpress.Remove(OnActionUnpress);
         }
 
-        public void Grabbed(Hand hand)
-        {
-            grabbedHand = hand;
-        }
-
-        public void Ungrabbed()
-        {
-            grabbedHand = Hand.Unknown;
-            // @todo Release all buttons when ungrabbed
-        }
-
         private void OnActionPress(ButtonActionsPress ev)
         {
-            if (grabbedHand == Hand.Unknown) return; // not grabbing
-            if (grabbedHand != ev.hand) return; // wrong hand
+            if (!IsValidHand(ev.hand)) return;
 
             if (joyBtnMap.ContainsKey(ev.button))
             {
                 uint btnIndex = joyBtnMap[ev.button];
-                if (output)
-                {
-                    output.SetButton(btnIndex, true);
-                }
+                PressButton(btnIndex);
             }
         }
 
         private void OnActionUnpress(ButtonActionsPress ev)
         {
-            if (grabbedHand == Hand.Unknown) return; // not grabbing
-            if (grabbedHand != ev.hand) return; // wrong hand
+            if (!IsValidHand(ev.hand)) return;
 
             if (joyBtnMap.ContainsKey(ev.button))
             {
                 uint btnIndex = joyBtnMap[ev.button];
-                if (output)
-                {
-                    output.SetButton(btnIndex, false);
-                }
+                UnpressButton(btnIndex);
             }
         }
     }
