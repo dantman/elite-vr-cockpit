ca2e2507b6799862c11a3051d7d01628ecc0a1f1
Daniel Friesen <d@danf.ca>
2019-02-24 10:18:06 -0800

    Implement a Y-axis controller

---
+++ Assets/Scripts/MapPlaneYMoveButton.cs
@@ -0,0 +1,13 @@
+﻿namespace EVRC
+{
+    public class MapPlaneYMoveButton : BaseButton
+    {
+        public MapPlaneController mapPlaneController;
+        public float setAxis;
+        protected override Unpress Activate()
+        {
+            mapPlaneController.SetYAxis(setAxis);
+            return () => mapPlaneController.SetYAxis(0);
+        }
+    }
+}
