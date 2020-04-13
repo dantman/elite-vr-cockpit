63a67f818682c1cfc64891549bd99ccc5afb1d8c
Daniel Friesen <d@danf.ca>
2020-04-12 20:37:49 -0700

    Implement new ED style UI text buttons

---
+++ Assets/Scripts/UI/TextButton.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace EVRC.UI
+{
+    /**
+     * Unity UI.Button subclass that also changes the text color of a button
+     */
+    [AddComponentMenu("EVRC/UI/Button", 30)]
+    public class TextButton : Button
+    {
+        public TextMeshProUGUI text;
+        public ColorBlock textColors;
+
+        protected override void DoStateTransition(SelectionState state, bool instant)
+        {
+            base.DoStateTransition(state, instant);
+            Color textColor;
+
+            switch (state)
+            {
+                case SelectionState.Highlighted:
+                    textColor = textColors.highlightedColor;
+                    break;
+                case SelectionState.Pressed:
+                    textColor = textColors.pressedColor;
+                    break;
+                case SelectionState.Selected:
+                    textColor = textColors.pressedColor;
+                    break;
+                case SelectionState.Disabled:
+                    textColor = textColors.disabledColor;
+                    break;
+                default:
+                    textColor = textColors.normalColor;
+                    break;
+            }
+
+            if (text != null)
+            {
+                text.color = textColor;
+                text.CrossFadeColor(textColor, (!instant) ? this.colors.fadeDuration : 0f, true, true);
+            }
+        }
+    }
+}
