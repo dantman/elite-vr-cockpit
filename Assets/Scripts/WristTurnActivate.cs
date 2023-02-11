38a3112776f393bfba0280e8096952d6344597da
boyestrous <jpboyes56@gmail.com>
2023-02-10 19:31:03 -0600

    added wrist menu
    WristTurnActivate watches for the correct combination of angles for the tracked hand and will activate a new game object that's attached to each hand

---
+++ Assets/Scripts/WristTurnActivate.cs
@@ -0,0 +1,33 @@
+﻿using EVRC;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class WristTurnActivate : MonoBehaviour
+{
+    private float zLow = 100.0f;
+    private float zHigh = 170.0f;
+    private float yLow = 235.0f;
+    private float yHigh = 295.0f;
+    public GameObject target;
+
+    void Update()
+    {
+        var euAngle = this.gameObject.transform.localRotation.eulerAngles;
+        if (euAngle.z > zLow && euAngle.z <= zHigh && euAngle.y > yLow && euAngle.y < yHigh)
+        {
+            if (!target.activeInHierarchy)
+            {
+                target.SetActive(true);
+            }
+            return;
+        }
+
+        if (target.activeInHierarchy)
+        {
+            target.SetActive(false);
+            return;
+        }
+    }
+}
