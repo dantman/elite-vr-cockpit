fc3693befa78d014a943677172631f32dd90dee5
boyestrous <jpboyes56@gmail.com>
2023-04-12 13:05:45 -0500

    GetStatusText abstract method

--- Assets/Core/GameStateSystem/GameState.cs
+++ Assets/Core/GameStateSystem/GameState.cs
@@ -6,6 +6,10 @@ namespace EVRC
 {
     public abstract class GameState : ScriptableObject
     {
+        /// <summary>
+        /// Flexible method for displaying the current state of an object/system.
+        /// </summary>
+        /// <returns>String representation of the Current Status</returns>
         public abstract string GetStatusText();
     }
 }
