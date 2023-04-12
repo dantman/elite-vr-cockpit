f6e69c9ec763bde954c9342501071b0ef881a90e
boyestrous <jpboyes56@gmail.com>
2023-04-12 16:36:36 -0500

    Core namespace tests
    first test makes sure the VJoyState class correctly returns a string for all enums. Essentially testing to make sure a string value has been defined.

---
+++ Assets/Core/Tests/VjoyStateTests.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using NUnit.Framework;
+using System.Reflection;
+using UnityEngine.TestTools;
+using System.Collections;
+
+namespace EVRC.Core.Tests
+{
+    public class VjoyStateTests
+    {
+        private VJoyState vJoyState;
+
+        [SetUp]
+        public void Setup()
+        {
+            vJoyState = ScriptableObject.CreateInstance<VJoyState>();
+        }
+
+
+        [Test]
+        public void GetStatusText_ForEachVJoyStatus_IsNotUnknown()
+        {
+            foreach (VJoyStatus status in System.Enum.GetValues(typeof(VJoyStatus)))
+            {
+                vJoyState.vJoyStatus = status;
+                string returnedStatus = vJoyState.GetStatusText();
+                if (status != VJoyStatus.Unknown)
+                {
+                    Assert.AreNotEqual("Unknown", returnedStatus);
+                }
+            }
+        }
+
+      
+    }
+}
