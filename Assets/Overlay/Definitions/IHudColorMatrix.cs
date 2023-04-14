f3bb0e5a6865d99dec65aa165438e3cd5178aa5e
boyestrous <jpboyes56@gmail.com>
2023-04-14 14:13:25 -0500

    reorganize folders

---
+++ Assets/Overlay/Definitions/IHudColorMatrix.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using EVRC.Core.Overlay;
+using UnityEngine;
+
+namespace EVRC.Core.Overlay
+{
+    public interface IHudColorMatrix
+    {
+        void SetColorMatrix();
+    }
+}
