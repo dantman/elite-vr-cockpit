50748f5031f3a0bfee8c592e455cccc179d1676e
boyestrous <jpboyes56@gmail.com>
2023-04-23 09:14:50 -0500

    replaced position tracking for HMD and Hands
    Tracked HMD still has a public Transform property, so the OverlayUtils can check IsFacingHMD
    Tracked Hands were completely replaced by the TrackedPoseDriver components

--- Assets/Overlay/Scripts/TrackedHMD.cs
+++ Assets/Overlay/Scripts/TrackedHMD.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.SpatialTracking;
 using Valve.VR;
 
 namespace EVRC.Core.Overlay
 {
+    [RequireComponent(typeof(TrackedPoseDriver))]
     public class TrackedHMD : MonoBehaviour
     {
+        private TrackedPoseDriver poseDriver;
+
         public static TrackedHMD _instance;
         public static TrackedHMD instance
         {
@@ -19,58 +24,24 @@ namespace EVRC.Core.Overlay
             get
             {
                 return instance.transform;
+                
             }
         }
             
-        SteamVR_Events.Action newPosesAction;
-
-        TrackedHMD()
-        {
-            newPosesAction = SteamVR_Events.NewPosesAction(OnNewPoses);
-        }
-
         void OnEnable()
         {
-            newPosesAction.enabled = true;
+            poseDriver = GetComponent<TrackedPoseDriver>();
+            poseDriver.SetPoseSource(TrackedPoseDriver.DeviceType.GenericXRDevice,
+                TrackedPoseDriver.TrackedPose.Center);
+            poseDriver.updateType = TrackedPoseDriver.UpdateType.UpdateAndBeforeRender;
+            poseDriver.trackingType = TrackedPoseDriver.TrackingType.RotationAndPosition;
         }
-
-        void OnDisable()
-        {
-            newPosesAction.enabled = false;
-
-            if (_instance == this)
-            {
-                _instance = null;
-            }
-        }
-
-        void OnNewPoses(TrackedDevicePose_t[] poses)
-        {
-            var deviceIndex = OpenVR.k_unTrackedDeviceIndex_Hmd;
-
-            if (poses.Length < deviceIndex) return;
-
-            if (!poses[deviceIndex].bDeviceIsConnected) return;
-
-            if (!poses[deviceIndex].bPoseIsValid) return;
-
-            var pose = new SteamVR_Utils.RigidTransform(poses[deviceIndex].mDeviceToAbsoluteTracking);
-            // When the application is using a seated universe convert it to a standing universe transform
-            if (OpenVR.Compositor.GetTrackingSpace() == ETrackingUniverseOrigin.TrackingUniverseSeated)
-            {
-                var seatedTransformMatrix = OpenVR.System.GetSeatedZeroPoseToStandingAbsoluteTrackingPose();
-                var seatedTransform = new SteamVR_Utils.RigidTransform(seatedTransformMatrix);
-                pose = seatedTransform * pose;
-            }
-
-            transform.localPosition = pose.pos;
-            transform.localRotation = pose.rot;
-        }
-
+        
+        
         void OnDrawGizmos()
         {
             Gizmos.matrix = transform.localToWorldMatrix;
-            Gizmos.color = Color.black;
+            Gizmos.color = Color.yellow;
             float aspectRatio = 16f / 9f;
             Gizmos.DrawFrustum(Vector3.zero, 110f / aspectRatio, 1f, .05f, aspectRatio);
         }
