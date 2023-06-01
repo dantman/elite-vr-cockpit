c53a9c7f951ea062367309acb007fb3bf7a43306
boyestrous <jpboyes56@gmail.com>
2023-06-01 07:18:21 -0500

    Set up cockpitmode anchor to activate/deactivate
    prepping for removing the cockpitmode enum in favor of a combo StatusFlag + Guifocus
    unit tests included for most common test cases
    old cockpitmode structure remains in place still

--- Assets/Core/Tests/CockpitModeAnchorTests.cs
+++ Assets/Core/Tests/CockpitModeAnchorTests.cs
@@ -7,40 +7,141 @@ using EVRC.Core;
 
 public class CockpitModeAnchorTests
 {
-    private static object[] ActivationCombinations =
-    {
-        new object[] { true, EDStatusFlags.InMainShip, new EDGuiFocus[] { EDGuiFocus.NoFocus, EDGuiFocus.InternalPanel, EDGuiFocus.ExternalPanel} },
-        new object[] { false, EDStatusFlags.InMainShip, new EDGuiFocus[] { EDGuiFocus.FSSMode } },
-        // Add more combinations as needed
-    };
+    GameObject parentGameObject;
+    CockpitModeAnchor cockpitModeAnchor;
+    GameObject childGameObject;
 
-    //[Test]
-    [Test, TestCaseSource(nameof(ActivationCombinations))]
-    public void OnEDStatusAndGuiChanged_ChildGameObjectBecomesActive(bool expectedOutcome, EDStatusFlags activationStatusFlag, EDGuiFocus[] activationGuiFocuses)
-    //public void OnEDStatusAndGuiChanged_ChildGameObjectBecomesActive()
+    /// <summary>
+    /// Configuration applied to test CockpitModeAnchor
+    /// </summary>
+    public struct anchorConfig
+    {
+        public EDStatusFlags statusFlag;
+        public EDGuiFocus guiFocus;
+    }
+
+    public struct testCase
+    {
+        public bool expectedOutcome;
+        // Applied to the test cockpitModeAnchor
+        public anchorConfig config;
+
+        // Settings for the event that will be raised in the test case
+        public EDStatusFlags raisedStatusFlag;
+        public EDGuiFocus raisedGuiFocus;
+    }
+
+    [SetUp]
+    public void SetUp()
     {
         // Create a new GameObject and attach the CockpitModeAnchor component
-        GameObject parentGameObject = new GameObject("Parent");
-        CockpitModeAnchor cockpitModeAnchor = parentGameObject.AddComponent<CockpitModeAnchor>();        
+        parentGameObject = new GameObject("Parent");
+        cockpitModeAnchor = parentGameObject.AddComponent<CockpitModeAnchor>();
 
         // Create a child GameObject
-        GameObject childGameObject = new GameObject("Child");
+        childGameObject = new GameObject("Child");
         childGameObject.transform.SetParent(parentGameObject.transform);
         childGameObject.SetActive(false);
         // This happens in OnEnable, but we need to force it here. Just make sure the child object is targeted
         cockpitModeAnchor.AddImmediateChildrenToList();
-        // Assert that the child GameObject is initially inactive
-        Assert.IsFalse(childGameObject.activeSelf);
+    }
 
-        // Set the activationStatusFlag and activationGuiFocuses fields
-        cockpitModeAnchor.activationStatusFlag = activationStatusFlag;
-        cockpitModeAnchor.activationGuiFocuses = activationGuiFocuses;
+    private static testCase[] ActivationTestCases =
+    {
+        // configured for FSS Mode, raised with FSS Mode Gui Focus and Mainship
+        new testCase() {
+            expectedOutcome=true,
+            config = new anchorConfig() { guiFocus = EDGuiFocus.FSSMode },
+            raisedStatusFlag = EDStatusFlags.InMainShip,
+            raisedGuiFocus = EDGuiFocus.FSSMode
+        },
+        
+        // Hypothetical future case where we want some special stuff to display when looking at one of the panels
+        new testCase() {
+            expectedOutcome=true,
+            config = new anchorConfig() { statusFlag = EDStatusFlags.InMainShip, guiFocus = EDGuiFocus.InternalPanel },
+            raisedStatusFlag = EDStatusFlags.InMainShip,
+            raisedGuiFocus = EDGuiFocus.InternalPanel
+        },
+        // No configuration provided. Should remain inactive
+        new testCase() {
+            expectedOutcome=false,
+            config = new anchorConfig() { },
+            raisedStatusFlag = EDStatusFlags.InSRV,
+            raisedGuiFocus = EDGuiFocus.NoFocus
+        },
+        // 
+        new testCase() {
+            expectedOutcome=false,
+            config = new anchorConfig() { },
+            raisedStatusFlag = EDStatusFlags.InSRV,
+            raisedGuiFocus = EDGuiFocus.NoFocus
+        },
+
+    };
+
+    /// <summary>
+    /// Test to confirm that objects which start deactivated are correctly activated when matching status/guifocus are raised
+    /// </summary>
+    [Test, TestCaseSource(nameof(ActivationTestCases))]
+    public void OnStatusChange_Activation(testCase testCase)
+    {
+        // Ensure that the child GameObject is initially inactive
+        if (childGameObject.activeSelf) childGameObject.SetActive(false);
+
+        // Set the activationStatusFlag and activationGuiFocuses fields for the test
+        cockpitModeAnchor.activationStatusFlag = testCase.config.statusFlag;
+        cockpitModeAnchor.activationGuiFocus = testCase.config.guiFocus;
 
 
-        // Call the OnEDStatusAndGuiChanged method with matching parameters and Assert that the child GameObject becomes active
-        cockpitModeAnchor.OnEDStatusAndGuiChanged(EDStatusFlags.InMainShip, EDGuiFocus.NoFocus);        
-        Assert.AreEqual(expectedOutcome, childGameObject.activeSelf);
+        // Call the OnEDStatusAndGuiChanged method with the raised parameters
+        cockpitModeAnchor.OnEDStatusAndGuiChanged(testCase.raisedStatusFlag, testCase.raisedGuiFocus);        
+        Assert.AreEqual(testCase.expectedOutcome, childGameObject.activeSelf);
+    }
+
+    private static testCase[] DeactivationTestCases =
+    {
+        // configured for FSS Mode, raised without FSS Mode Gui Focus
+        new testCase() {
+            expectedOutcome=false,
+            config = new anchorConfig() { guiFocus = EDGuiFocus.FSSMode },
+            raisedStatusFlag = EDStatusFlags.InMainShip,
+            raisedGuiFocus = EDGuiFocus.NoFocus
+        },
+        // configured for MainShip, raised with FSS Mode Gui Focus and Mainship
+        new testCase() {
+            expectedOutcome=false,
+            config = new anchorConfig() { statusFlag = EDStatusFlags.InMainShip },
+            raisedStatusFlag = EDStatusFlags.InMainShip,
+            raisedGuiFocus = EDGuiFocus.FSSMode
+        },
+        // Hypothetical future case where we want some special stuff to display when looking at one of the panels
+        // raised status/focus don't match, so should deactivate
+        new testCase() {
+            expectedOutcome=false,
+            config = new anchorConfig() { statusFlag = EDStatusFlags.InMainShip, guiFocus = EDGuiFocus.InternalPanel },
+            raisedStatusFlag = EDStatusFlags.InMainShip,
+            raisedGuiFocus = EDGuiFocus.ExternalPanel
+        },
+
+    };
+
+    /// <summary>
+    /// Test to confirm that objects which start active are correctly deactivated
+    /// </summary>
+    [Test, TestCaseSource(nameof(DeactivationTestCases))]
+    public void OnStatusChange_Deactivation(testCase testCase)
+    {
+        // Ensure that the child GameObject is initially Active
+        if (!childGameObject.activeSelf) childGameObject.SetActive(true);
+
+        // Set the activationStatusFlag and activationGuiFocuses fields for the test
+        cockpitModeAnchor.activationStatusFlag = testCase.config.statusFlag;
+        cockpitModeAnchor.activationGuiFocus = testCase.config.guiFocus;
 
 
+        // Call the OnEDStatusAndGuiChanged method with the raised parameters
+        cockpitModeAnchor.OnEDStatusAndGuiChanged(testCase.raisedStatusFlag, testCase.raisedGuiFocus);
+        Assert.AreEqual(testCase.expectedOutcome, childGameObject.activeSelf);
     }
 }
--- Assets/Overlay/Scripts/CockpitModeAnchor.cs
+++ Assets/Overlay/Scripts/CockpitModeAnchor.cs
@@ -19,7 +19,7 @@ namespace EVRC.Core.Overlay
     {
         public CockpitMode cockpitUiMode = CockpitMode.Cockpit;
         [Tooltip("The single status flag that must be present for this Object to be active")] public EDStatusFlags activationStatusFlag;
-        [Tooltip("Which GUI Focuses must be present for this Object to be active")] public EDGuiFocus[] activationGuiFocuses;
+        [Tooltip("Which GUI Focus must be present for this Object to be active. Null means any focus will work")] public EDGuiFocus activationGuiFocus;
 
         [Tooltip("The object that contains the child objects for a cockpitMode"), SerializeField]
         private List<GameObject> targets;
@@ -53,10 +53,39 @@ namespace EVRC.Core.Overlay
         public void OnEDStatusAndGuiChanged(EDStatusFlags newStatusFlags, EDGuiFocus newGuiFocus)
         {
             Debug.Log($"On EDStatusAndGuiChanged activated with: {newStatusFlags} | {newGuiFocus}");
+            // the default(Enum) convention means nothing has been selected in the inspector, so they are (effectively) null
+            if (activationGuiFocus == default(EDGuiFocus) && activationStatusFlag == default(EDStatusFlags)) return;
 
-            bool shouldActivate = newStatusFlags.HasFlag(activationStatusFlag) && activationGuiFocuses.Contains(newGuiFocus);
+            // evaluate guifocus first b/c they are more specific than status flags
+            if (activationGuiFocus != default(EDGuiFocus))
+            {
+                ActivateTargets(newGuiFocus == activationGuiFocus);
+                // we only evaluate the status flag if the guifocus is null
+                return;
+            }
 
-            // activate all targets (children) if the status has a matching flag and the GUI focus list contains the current value.
+            // These 5 GuiFocus values are the ones that indicate a special view in which other modes should be deactivated
+            // FSS Mode, for example, removes the traditional cockpit view in the game. So the player can't see their joystick, throttle, or any information panels.
+            EDGuiFocus[] guiAnchors = new EDGuiFocus[5] {EDGuiFocus.GalaxyMap, EDGuiFocus.FSSMode, EDGuiFocus.SystemMap, EDGuiFocus.StationServices, EDGuiFocus.Orrery};
+
+            // If a guifocus wasn't defined (checked above) AND it's one of the special ones, the targets will always be deactivated
+            if (guiAnchors.Contains(newGuiFocus))                
+            {
+                ActivateTargets(false);
+                return;
+            }
+
+
+            //Only after evaluating the guiFocus, check status flags for match
+            if (activationStatusFlag != default(EDStatusFlags))
+            {
+                // Activate if the activation flag is present
+                ActivateTargets(newStatusFlags.HasFlag(activationStatusFlag));
+            }
+        }
+
+        private void ActivateTargets(bool shouldActivate) 
+        {
             foreach (var target in targets)
                 target.SetActive(shouldActivate);
         }
